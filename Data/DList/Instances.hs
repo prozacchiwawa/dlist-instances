a48a1285e17b070a55d2a67da219364b45201286
arty <arty@banyaninfrastructure.com>
2019-09-14 17:22:34 -0700

    Eta

--- Data/DList/Instances.hs
+++ Data/DList/Instances.hs
@@ -4,9 +4,11 @@ module Data.DList.Instances () where
 
 -- don't define instance for dlist>=0.8 && base>=4.9
 #if !(MIN_VERSION_base(4,9,0)) || !(MIN_VERSION_dlist(0,8,0))
+#ifndef ETA_VERSION
 import Data.DList
 import Data.Semigroup
 
 instance Semigroup (DList a) where
   (<>) = append
+#endif
 #endif
